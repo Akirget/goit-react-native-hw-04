20c0c98d020553312c204c0c55123376e68bc38e
Kirhet Oleksandr <kirget.sasha2002@gmail.com>
2023-02-14 09:56:56 +0100

    Домашнє завдання №4 React Native

--- App.js
+++ App.js
@@ -1,11 +1,18 @@
-import LoginScreen from "./Screens/components/LoginScreen/LoginScreen";
-import RegistrationScreen from "./Screens/components/RegistrationScreen/RegistrationScreen";
+import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer } from "@react-navigation/native";
+
+import LoginScreen from "./Screens/Auth/LoginScreen/LoginScreen";
+import RegistrationScreen from "./Screens/Auth/RegistrationScreen/RegistrationScreen";
+
+const AuthStack = createStackNavigator();
 
 export default function App() {
   return (
-    <>
-      {/* <RegistrationScreen /> */}
-      <LoginScreen />
-    </>
+    <NavigationContainer>
+      <AuthStack.Navigator>
+        <AuthStack.Screen name="Login" component={LoginScreen} />
+        <AuthStack.Screen name="Register" component={RegistrationScreen} />
+      </AuthStack.Navigator>
+    </NavigationContainer>
   );
 }
